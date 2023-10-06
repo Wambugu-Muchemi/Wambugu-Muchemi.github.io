7ff8c1f7bf9076a60df9b27650ea49d077fae966
Wambugu Muchemi <wambugumuchemi2021@gmail.com>
2023-10-06 13:58:57 +0300

    new js

--- main.js
+++ main.js
@@ -1,12 +1,63 @@
-var anime = require('animejs');
+function animateElements() {
+  // Code to animate elements here
+}
 
-// Create a new anime object
-var typingAnimation = anime({
-  targets: 'h1',
-  text: 'Data Scientist',
-  duration: 2000,
-  easing: 'easeInOutCubic'
+document.addEventListener('DOMContentLoaded', function() {
+  animateElements();
 });
 
-// Start the animation
-typingAnimation.play();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+function fadeInOnScroll() {
+  const elements = document.querySelectorAll('.fade-in');
+
+  elements.forEach(element => {
+    const isInViewport = (elementTop, elementBottom, viewportTop, viewportBottom) => {
+      return (elementTop <= viewportBottom) && (elementBottom >= viewportTop);
+    };
+
+    const elementTop = element.getBoundingClientRect().top;
+    const elementBottom = element.getBoundingClientRect().bottom;
+    const viewportTop = window.scrollY;
+    const viewportBottom = viewportTop + window.innerHeight;
+
+    if (isInViewport(elementTop, elementBottom, viewportTop, viewportBottom)) {
+      element.classList.add('fade-in-visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', fadeInOnScroll);
+
+const sidebarMenu = document.querySelector('.sidebar-menu');
+const sidebarToggleButton = document.querySelector('.sidebar-toggle-button');
+
+sidebarToggleButton.addEventListener('click', () => {
+  sidebarMenu.classList.toggle('expanded');
+});
+
+const contactForm = document.querySelector('#contact-form');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Validate form fields and display relevant error messages
+  // ...
+
+  // If form is valid, submit the form
+  contactForm.submit();
+});
+
+function initialize() {
+  // Initialization code here
+}
+
+document.addEventListener('DOMContentLoaded', initialize);
